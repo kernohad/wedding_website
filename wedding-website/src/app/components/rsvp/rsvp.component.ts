c7524ddb44a3e43f4280b204df69b38bb3687af7
Unknown <kernohad@mail.gvsu.edu>
2019-06-07 20:25:33 -0400

    remove embedded rsvpify attempt

--- wedding-website/src/app/components/rsvp/rsvp.component.ts
+++ wedding-website/src/app/components/rsvp/rsvp.component.ts
@@ -1,5 +1,4 @@
-import { Component, Renderer2, OnInit, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rsvp',
@@ -8,31 +7,9 @@ import { DOCUMENT } from '@angular/common';
 })
 export class RsvpComponent implements OnInit {
 
-  constructor(
-    private _renderer2: Renderer2, 
-    @Inject(DOCUMENT) private _document: Document
-  ) { }
+  constructor() { }
 
   public ngOnInit() {
-
-      let s = this._renderer2.createElement('script');
-      s.type = `text/javascript`;
-      s.src = `http://app.rsvpify.com/embed/500182186`;
-      s.scrolling = 'yes';
-
-      this._renderer2.appendChild(this._document.body, s);
-
-      let d = this._renderer2.createElement('script');
-      d.type = `text/javascript`;
-      d.src = `http://app.rsvpify.com/js/iframeResizer.min.js`;
-
-      this._renderer2.appendChild(this._document.body, d);
-
-      let x = this._renderer2.createElement('script');
-      x.type = `text/javascript`;
-      x.innerHTML = `iFrameResize({ autoResize: true, checkOrigin: false, heightCalculationMethod: 'max', enablePublicMethods: true}, "#RSVPifyIFrame");`;
-
-      this._renderer2.appendChild(this._document.body, x);
   }
 
 }
